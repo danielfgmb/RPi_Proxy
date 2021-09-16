c5db99e640bfc296b0f41a3d82ef0bc899f17ab4
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-09-16 19:25:53 +0100

    Update

--- main_V2.py
+++ main_V2.py
@@ -3,8 +3,18 @@ import json
 
 SERVER = "194.210.159.33"
 
+PORT = "8000"
+FORMAT = 'utf-8'
 
-api_url = "http://"+SERVER+":8000/api/v1/apparatus/2/3/config"
+APPARATUS_ID = "2"
+EXPERIMENT_ID = "3"
+
+# CONFIG_OF_EXP = []
+MY_IP = "192.168.1.83"
+SEGREDO = "estou bem"
+SAVE_DATA = []
+
+api_url = "http://"+SERVER+":"+PORT+"/api/v1/apparatus/"+APPARATUS_ID+"/"+EXPERIMENT_ID+"/config"
 response =  requests.post(api_url)
 CONFIG_OF_EXP = response.json()
 print(json.dumps(CONFIG_OF_EXP,indent=4))
