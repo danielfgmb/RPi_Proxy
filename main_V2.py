9dbe8d510410b14ec729ffa8f7df5c3dd0a1279e
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-09-19 11:17:18 +0100

    Update server

--- main_V2.py
+++ main_V2.py
@@ -1,7 +1,9 @@
 import requests
 import json
 from datetime import datetime
-
+import importlib
+import threading
+import time
 
 
 # SERVER = "194.210.159.33"
@@ -19,7 +21,53 @@ MY_IP = "192.168.1.83"
 SEGREDO = "estou bem"
 SAVE_DATA = []
 
+interface = None
 
+
+def send_exp_data():
+    global SAVE_DATA
+    while interface.receive_data_from_exp() != "DATA_START":
+        pass
+    send_message = {"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"value":"","result_type":"p","status":"Experiment Starting"}
+    SendPartialResult(send_message)
+    while True:
+        exp_data = interface.receive_data_from_exp()
+        print(exp_data)
+        if exp_data != "DATA_END":
+            SAVE_DATA.append('{"timestamp":"'+str(time.time_ns())+'","Data":'+str(exp_data)+'}')
+            send_message = {"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"value":str(exp_data),"result_type":"p","status":"running"}
+            SendPartialResult(send_message)
+        else:
+            send_message = {"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"value":"","result_type":"p","status":"Experiment Ended"}
+            SendPartialResult(send_message)
+            send_message = {"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"value":str(SAVE_DATA).replace('\'', ''),"result_type":"f"}
+            SendPartialResult(send_message)
+            return 
+
+
+def Send_Config_to_Pic(myjson):
+    print("Recebi mensagem de configurestart. A tentar configurar pic")
+    actual_config, config_feita_correcta = interface.do_config(myjson)
+    if config_feita_correcta :   #se config feita igual a pedida? (opcional?)
+        data_thread = threading.Thread(target=send_exp_data,daemon=True)
+        print("PIC configurado.\n")
+        if interface.do_start():                            #tentar começar experiencia
+            print("aqui")
+            data_thread.start()
+            time.sleep(0.000001)
+            #O JSON dos config parameters está mal e crasha o server. ARRANJAR
+            #send_mensage = '{"reply_id": "2","status":"Experiment Running","config_params":"'+str(myjson["config_params"])+'}'
+            send_mensage = {"reply_id": "2","status":"Experiment Running"}
+        else :
+            send_mensage = {"reply_id": "2", "error":"-1", "status":"Experiment could not start"}
+    
+    else:
+        send_mensage = {"reply_id": "2", "error":"-2", "status":"Experiment could not be configured"}
+    return send_mensage
+
+
+
+# REST
 def GetConfig():
     api_url = "http://"+SERVER+":"+PORT+"/api/v1/apparatus/"+APPARATUS_ID+"/"+EXPERIMENT_ID+"/config"
     msg = {"secret":SEGREDO}
@@ -36,19 +84,67 @@ def GetExecution():
     print(json.dumps(next_execution,indent=4))
     return ''
 
-def SendPartialResult():
+def SendPartialResult(msg):
     global next_execution
     print(next_execution)
     api_url = "http://"+SERVER+":"+PORT+"/api/v1/sendpartialresult/"+str(next_execution["execution_id"])
-    todo = {"value":{"ok":"ola","ponto":"oco"},"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"result_type":"p"}
-    response =  requests.post(api_url, json=todo)
+    # todo = {"value":{"ok":"ola","ponto":"oco"},"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"result_type":"p"}
+    response =  requests.post(api_url, json=msg)
     Result_id = response.json()
     print(json.dumps(Result_id,indent=4))
     return ''
 
 
+# if __name__ == "__main__":
+#     GetConfig()
+#     GetExecution()
+#     print(json.dumps(next_execution,indent=4))
+#     SendPartialResult()
+
+def main_cycle():
+    global next_execution
+    if CONFIG_OF_EXP != None:
+        while True:
+            while next_execution["config_params"] != None:
+                GetExecution()
+            status_config=Send_Config_to_Pic(next_execution["config_params"])
+            next_execution = None
+            if status_config ["status"] == "Experiment Running":
+                send_exp_data()
+
+    return ''
+
+
 if __name__ == "__main__":
-    GetConfig()
-    GetExecution()
-    print(json.dumps(next_execution,indent=4))
-    SendPartialResult()
+    print("[Starting] Experiment Server Starting...")
+    # global next_execution
+    connected = None
+    interface = importlib.import_module("pic_interface.interface")
+    while True:
+        try:
+            GetConfig()
+            if interface.do_init(CONFIG_OF_EXP) :
+                main_cycle()
+            else:
+                print ("Experiment not found")
+        except:
+            #LOG ERROR
+            print("Faill to connect to the Server. Trying again after 10 s")
+            #So faz shutdown do socket se este chegou a estar connected
+            time.sleep(10)
+
+
+
+# global CONFIG_OF_EXP
+# global interface
+# interface = importlib.import_module("pic_interface.interface")
+# print("Recebi mensagem de configuracao. A tentar inicializar a experiencia\n")
+# CONFIG_OF_EXP = myjson['config_file']
+
+# #LIGAR A DISPOSITIVO EXP - INIT
+# #Talvez passar erros em forma de string JSON para incluir no reply em vez de OK e NOT OK
+# if interface.do_init(CONFIG_OF_EXP) :
+#     send('{"reply_id": "1", "status":"Experiment initialized OK"}')
+
+# else :
+#     send('{"reply_id": "1", "error":"-1", "status":"Experiment initialized NOT OK"}')
