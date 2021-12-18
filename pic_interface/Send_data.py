cc09d3be245a88d2640d22eef10a979dae67de5a
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-18 22:14:34 +0000

    Update Cavity code added

---
+++ pic_interface/Send_data.py
@@ -0,0 +1,21 @@
+import requests
+
+
+
+def SendPartialResult(config,msg):
+    global next_execution
+    # print(next_execution)
+    print(str(msg))
+    HEADERS = { 
+    "Authentication": str(config['DEFAULT']['SECRET']), 
+    "Content-Type": "application/json"
+    }
+    api_url = "http://"+config['DEFAULT']['SERVER']+":"+config['DEFAULT']['PORT']+"/api/v1/result"
+    print(api_url)
+    # todo = {"value":{"ok":"ola","ponto":"oco"},"time":datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),"result_type":"p"}
+    print("Aqui:  " ,json.dumps(msg,indent=4))
+    requests.post(api_url, headers = HEADERS, json=msg)
+    # Result_id = response.json()
+    # if (test_end_point_print):
+    #     print(json.dumps(Result_id,indent=4))   
+    return ''
