f087118a8d5f70d6d6207b016fe92f804505838d
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-19 17:00:14 +0000

    Update Cavity code added

--- pic_interface/Cavity.py
+++ pic_interface/Cavity.py
@@ -45,7 +45,7 @@ def Set_Up_Exp(gas_select,gas_amount):
     GPIO.Vacum_Pump_stat(ON)
     time.sleep(5)
     GPIO.Valve_cut_off_stat(ON)
-    time.sleep(40)
+    time.sleep(120)
     # wait untly pressure is less them press_back
     GPIO.Valve_cut_off_stat(OFF)
     GPIO.Inject_Gas(gas_select, gas_amount)
