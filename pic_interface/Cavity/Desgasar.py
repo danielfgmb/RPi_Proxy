7775073ee4b5ed8766065c9b7bad6d18423fdf1b
PedroRossa98 <pedro.rossa@tecnico.ulisboa.pt>
2021-12-27 17:04:36 +0000

    Update with new intreface

--- pic_interface/Cavity/Desgasar.py
+++ pic_interface/Cavity/Desgasar.py
@@ -24,10 +24,12 @@ if __name__ == "__main__":
     # Set Up experiment:
     M_C.Vacum_Pump_stat(1)
     M_C.Inject_Gas(1,0.006)
+    M_C.Discharge_stat(1)
     while True:
         if input() == '0':
            stop=0
            M_C.Vacum_Pump_stat(0)
+           M_C.Discharge_stat(0)
            break
         else:
             pass 
