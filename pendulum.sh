d5958a539b764898e30e655dc6334d354629450e
Daniel Fernando Gomez Barrera <df.gomezb@uniandes.edu.co>
2022-09-27 16:28:58 -0500

    xd2

--- pendulum.sh
+++ pendulum.sh
@@ -8,15 +8,18 @@
 if ! pgrep -x "openvpn" > /dev/null
 then
     # editar para concuerde con archivo pendulo
+    echo activando openvpn 
     sudo openvpn /home/pi/pendulo1.ovpn > /dev/null 2>&1 &
 fi
 
 if ! pgrep -f "python3 main.py" > /dev/null
 then
+    echo activando pendulo 
     sudo python3 main.py > /dev/null 2>&1 &
 fi
 
 if ! pgrep -x "ffmpeg" > /dev/null
 then
+    echo activando transmision
     sh ./video-stream.sh > /dev/null 2>&1 &
 fi
