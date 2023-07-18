0e3d3e3d8df934371880a493ac6085db182a7f5f
Daniel Gomez <df.gomezb@uniandes.edu.co>
2023-07-18 11:15:37 -0500

    cambios finales

--- pendulum.sh
+++ pendulum.sh
@@ -4,6 +4,8 @@
 # -x flag only match processes whose name (or command line if -f is
 # specified) exactly match the pattern. 
 
+. ./datos_servidor.ini
+
 echo `date`
 echo v10
 # ruta
@@ -13,7 +15,7 @@ if ! pgrep -x "openvpn" > /dev/null
 then
     # editar para concuerde con archivo pendulo
     echo activando openvpn 
-    sudo openvpn /home/pi/pendulo1.ovpn > /dev/null 2>&1 &
+    sudo openvpn $ubicacion_ovpn_file > /dev/null 2>&1 &
     echo durmiendo por 30s
     sleep 20
 else
@@ -24,7 +26,7 @@ if ! pgrep -x "python3" > /dev/null
 then
     echo activando pendulo 
     killall python3
-    cd /home/pi/RPi_Proxy/ && python3 main.py > /dev/null 2>&1 &
+    cd $ubicacion_proxy  && python3 main.py > /dev/null 2>&1 &
 
 else
     echo pendulo corriendo
@@ -32,8 +34,11 @@ fi
 
 if ! pgrep -x "ffmpeg" > /dev/null
 then
-    echo activando transmision
-    cd /home/pi/RPi_Proxy/ && sudo nohup sh video-stream.sh > /dev/null 2>&1 &
+    echo nada ffmpeg $video_frame $video_width\x$video_height $usb_camera $video_server/$stream_key
+    sudo ffmpeg -f v4l2 -framerate $video_frame -video_size $video_width\x$video_height -i $usb_camera -c:v libx264 -preset veryfast -tune zerolatency -pix_fmt yuv420p -an -f flv $video_server/$stream_key > /dev/null 2>&1 &
+
+    # echo activando transmision
+    # cd /home/pi/RPi_Proxy/ && sudo nohup sh video-stream.sh > /dev/null 2>&1 &
 else
     echo ffmpeg corriendo
 fi
